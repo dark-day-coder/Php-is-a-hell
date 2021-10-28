8e230dff46d797a65d7dabdfe6473e078868da29
dark-day-coder <darkday.coder@gmail.com>
2021-10-28 23:45:17 +0545

    file for user data created

---
+++ user.php
@@ -0,0 +1,8 @@
+<?php
+    $user = [
+            'name' => 'sibu',
+            'username' => '@besibu',
+            'email' => 'darkday.coder@gmail.com',
+            'password' => '@@hell'
+        ];
+?>
